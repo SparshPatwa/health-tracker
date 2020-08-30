026540053a7a6b3f729e43a32a0520304a61d544
Sparsh Patwa <patwa.sparsh@gmail.com>
2020-08-30 03:50:40 -0700

    Add model abstraction for Calorie and Exercise tables

---
+++ models/Exercise.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../util/connect');
+
+class Exercise extends Model { }
+
+Exercise.init(
+{
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    calorie_outake: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    track_type: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+            min: 0, // This represents GOAL
+            max: 1 // This represents ACHIEVEMENT
+        }
+    },
+    record_date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'exercise'
+}
+);
+
+module.exports = Exercise;
