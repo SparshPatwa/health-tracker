0c2d59e8b275402f1eb1313dbca62eda68c99099
Sparsh Patwa <patwa.sparsh@gmail.com>
2020-09-01 20:23:39 -0700

    Add DB config for Heroku JAWSDB

--- util/connect.js
+++ util/connect.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 // DB configuration
-const sequelize = new Sequelize(
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
