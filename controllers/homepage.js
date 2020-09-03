9defdb62a24164557a3d984a81f58759b51104a1
armydavid <65372984+armydavid@users.noreply.github.com>
2020-09-02 20:59:22 -0400

    Push4

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -6,40 +6,73 @@ const { User } = require('../models');
 // Render user home if signed-in, else render login page; endpoint=/
 router.get('/', (req, res) => {
     if (req.session.loggedIn) {
-        res.render('userhome');
+        res.redirect('/userhome');
         return;
-      }
+    }
+    res.redirect('/about');
+});
+
+router.get('/about', (req, res) => {
+    if (req.session.loggedIn) {
+        return res.redirect('/');
+    }
+    res.render('about');
+});
+
+router.get('/team', (req, res) => {
+    if (req.session.loggedIn) {
+        return res.redirect('/');
+    }
+    res.render('team');
+});
+
+router.get('/login', (req, res) => {
     res.render('login');
 });
+
+router.get('/save-goal', (req, res) => {
+    if (!req.session.loggedIn) {
+        return res.redirect('/');
+    }
+    res.render('save-goal', {
+        loggedIn: true
+    });
+});
+
+router.get('/userhome', (req, res) => {
+    res.render('userhome', {
+        loggedIn: true
+    });
+})
 
 // Redirect to `/` if signed-in, else render singup page; endpoint=/signup/ 
 router.get('/signup', (req, res) => {
     if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
+        res.redirect('/');
+        return;
     }
     res.render('signup');
 });
 
 // Redirect to `/` in not signed in, else render account info page; endpont=/accountinfo/
 router.get('/accountinfo', (req, res) => {
-  if (!req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-  // Render accountinfo page with user information ussing user id saved in session
-  User.findOne({
-    where: { id: req.session.user_id },
-    attributes: { exclude: ['password', 'id'] }
-  })
-  .then(dbPostData => {
-    const user = dbPostData.get({ plain: true });
-    res.render('accountinfo', {user});
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    if (!req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    // Render accountinfo page with user information ussing user id saved in session
+    User.findOne({
+            where: { id: req.session.user_id },
+            attributes: { exclude: ['password', 'id'] }
+        })
+        .then(dbPostData => {
+            const user = dbPostData.get({ plain: true });
+            res.render('accountinfo', { loggedIn: true, user });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
--- public/javascript/accountActions.js
+++ public/javascript/accountActions.js
@@ -1,16 +1,23 @@
 async function logout() {
     const response = await fetch('/api/user/logout', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' }
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
     });
-  
+
     if (response.ok) {
-      document.location.replace('/');
+        document.location.replace('/');
     } else {
-      alert(response.statusText);
+        alert(response.statusText);
     }
 }
 
-async function deleteAccount(){
-  console.log("Not Implemented");
+async function deleteAccount() {
+    console.log("Not Implemented");
 }
+
+
+document.querySelector("#saveGoal").addEventListener('click', function(e) {
+    e.preventDefault();
+    let watergoal = document.querySelector('input[name="water-goal"]').value;
+    console.log("water", watergoal);
+})
